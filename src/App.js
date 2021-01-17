ae6fdefe12a9928d6fafd6d332223476f138f1c9
r5liang <richard.liang.583@gmail.com>
2021-01-17 00:44:43 -0800

    personalized

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
+import angery_react from './angery_react.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={angery_react} className="App-logo" alt="angery react" />
         <div>
           This is Richard's website.
         </div>
-        <div>
+        <div className="App-horiz-layout">
           <a
             className="Profile-link"
             href="https://www.linkedin.com/in/richardrliang/"
             target="_blank"
+            rel="noreferrer"
           >
             LinkedIn
-          </a> <a
+          </a>
+          <a
             className="Profile-link"
             href="https://www.facebook.com/richard.liang.583/"
             target="_blank"
+            rel="noreferrer"
           >
             Facebook
+          </a>
+          <a
+            className="Profile-link"
+            href="https://twitter.com/angery_rliang"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
           </a>
         </div>
       </header>
