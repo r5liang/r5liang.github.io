3393075acc946799c1ad9eb5fdf4b2cb1cc63276
r5liang <richard.liang.583@gmail.com>
2021-01-17 04:00:02 -0800

    added localization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+// @flow
+
 import angery_react from './angery_react.jpg';
 import './App.css';
+import {useState} from 'react';
+import * as React from 'react';
+import EN from './en.json';
+import ZH from './zh.json';
+import JP from './jp.json';
+import LanguageSwitch from './LanguageSwitch';
 
-function App() {
+export type Language = "en" | "zh" | "jp";
+
+function App(): React.Node {
+  const [language, setLanguage] = useState("en");
+  
+  const localizeHelper = (key: string) => {
+      switch (language) {
+        case "en":
+          return EN[key];
+        case "zh":
+          return ZH[key];
+        case "jp":
+          return JP[key];
+        default:
+          throw new Error("wtf must be tripping");
+      }
+  }
+  // fall back to English if a translation is missing
+  const localize = (key: string) => {
+    return localizeHelper(key) || EN[key];
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={angery_react} className="App-logo" alt="angery react" />
         <div>
-          This is Richard's website.
+          {localize("intro")}
         </div>
         <div className="App-horiz-layout">
           <a
@@ -16,7 +45,7 @@ function App() {
             target="_blank"
             rel="noreferrer"
           >
-            LinkedIn
+            {localize("linkedin")}
           </a>
           <a
             className="Profile-link"
@@ -24,7 +53,7 @@ function App() {
             target="_blank"
             rel="noreferrer"
           >
-            Facebook
+            {localize("facebook")}
           </a>
           <a
             className="Profile-link"
@@ -32,8 +61,13 @@ function App() {
             target="_blank"
             rel="noreferrer"
           >
-            Twitter
+            {localize("twitter")}
           </a>
+        </div>
+        <div className="App-horiz-layout">
+          <LanguageSwitch currLanguage={language} newLanguage="en" setLanguage={setLanguage} />
+          <LanguageSwitch currLanguage={language} newLanguage="zh" setLanguage={setLanguage} />
+          <LanguageSwitch currLanguage={language} newLanguage="jp" setLanguage={setLanguage} />
         </div>
       </header>
     </div>
